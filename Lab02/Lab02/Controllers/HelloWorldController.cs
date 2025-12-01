74875d6923ce1f6d4f8207a2ae80c0ee3c27a72a
LAB12-PC35 <LAB12-PC35@LAB12-PC35>
2025-12-01 11:16:56 +0700

    Add project files.

---
+++ Lab02/Lab02/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Lab02.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "this is my default action";
+        }
+        public string Welcome(string name, int numTimes =1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, numTimes is : {numTimes}");
+        }
+    }
+}
