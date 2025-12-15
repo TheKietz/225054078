221a3708267202a74e2fcfdcaf7b221827b28069
the kiet <thekiet1109@gmail.com>
2025-12-15 11:50:33 +0700

    Lab07 init

---
+++ Lab07/Lab07/Data/UEFContext.cs
@@ -0,0 +1,24 @@
+﻿using Lab07.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab07.Data
+{
+    public class UEFContext : DbContext
+    {
+        public UEFContext(DbContextOptions<UEFContext> options) : base(options)
+        {
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                .ToTable("Student");
+            modelBuilder.Entity<Enrollment>()
+                .ToTable("Enrollment");
+            modelBuilder.Entity<Course>()
+                .ToTable("Course");
+        }
+    }
+}
---
+++ Lab07/Lab07/Program.cs
@@ -0,0 +1,46 @@
+using Lab07.Data;
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<UEFContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("UEFContext") ?? 
+    throw new InvalidOperationException("Connection string 'UEFContext' not found.")));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<UEFContext>();
+        context.Database.EnsureCreated();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
